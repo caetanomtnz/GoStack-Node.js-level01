5780276f5e0925c85204bd596780fab47511d396
caetanomtnz <caetanomartinazzo25@gmail.com>
2021-06-05 20:49:16 -0400

    Add files via upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,30 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'hello'});
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
 });
 
-app.listen(3333,()=>{
+app.post('/projects', (request, response)=>{
+   return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+]);
+});
+
+app.put('/projects/:id', (request, response)=>{
+    return response.json([
+     'Projeto 2',
+     'Projeto 3',
+ ]);
+ });
+
+app.listen(3333,() =>{
     console.log('Back-end started')
 });
