324edd45c8eb38b368bd26a5013e5f9c2f560675
caetanomtnz <caetanomartinazzo25@gmail.com>
2021-06-06 13:43:24 -0400

    Add files via upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,46 +1,67 @@
 const { request, response } = require('express');
 const express = require('express');
+const { uuid }= require('uuidvd')
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/projects', (request, response) => {
-   const {title, owner} = request.query;
 
-   console.log(owner)
-   console.log(title)
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
+const projects = []
+
+app.get('/projects', (request, response) => {
+  const {title} = request.query;
+
+   const results= title
+   ? projects.filter(project=> project.title.includes(title))
+   : projects;
+    return response.json(projects)
 });
 
 app.post('/projects', (request, response)=>{
   const {title, owner} = request.body
 
-  console.log(owner);
-  console.log(title);
+   const project = {id:uuid(),title, owner}
 
+projects.push(project)
   
-    return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-]);
+    return response.json(project)
 });
 
 app.put('/projects/:id', (request, response)=>{
     const { id } = request.params
+    const {title, owner} = request.body
 
-    console.log(params)
+    const projectIndex=projects.findIndex(project=>project.id===id)
     
-    return response.json([
-     'Projeto 2',
-     'Projeto 3',
- ]);
+    if(projectIndex<0){
+        return response.status(400).json({error:'project not found'})
+    }
+
+    const project ={
+        id,
+        title,
+        owner
+    }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
  });
 
+app.delete('/projects/:id', (request, response)=>{
+    const{ id } = request.params
+
+    const projectIndex=projects.findIndex(project=>project.id===id)
+    
+    if(projectIndex<0){
+        return response.status(400).json({error:'project not found'})
+    }
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
+})
+
 app.listen(3333,() =>{
-    console.log('Back-end started')
+    console.log('🚀Back-end started')
 });
